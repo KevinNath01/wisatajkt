30f14106b3f7ebca3727ff6d22c5a0ec2ba14261
Kevin Nathaniel <kevinnathan010@gmail.com>
2021-05-28 19:03:24 +0700

    Add files via upload

--- application/config/database.php
+++ application/config/database.php
@@ -75,6 +75,28 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+	'hostname' => 'remotemysql.com',
+	'username' => 'eqQ2h4bQJL',
+	'password' => '3HDo5h7ztE',
+	'database' => 'eqQ2h4bQJL',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+/*$db['default'] = array(
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
@@ -93,4 +115,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+);*/

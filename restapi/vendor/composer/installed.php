56b4748e38252da8ef3c5f80bc2da59b3c8f2907
Kevin_Nathan <kevinnathan011@gmail.com>
2021-05-28 18:32:56 +0700

    Composer update

---
+++ restapi/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '91b67fa744189923e714a0d2b31ae831361b594f',
+    'name' => 'chriskacerguis/codeigniter-restserver',
+  ),
+  'versions' => 
+  array (
+    'chriskacerguis/codeigniter-restserver' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '91b67fa744189923e714a0d2b31ae831361b594f',
+    ),
+    'codeigniter/framework' => 
+    array (
+      'pretty_version' => '3.1.11',
+      'version' => '3.1.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b73eb19aed66190c10c9cad476da7c36c271d6dc',
+    ),
+  ),
+);
